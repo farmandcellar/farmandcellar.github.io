6d59b21115c15c0784022a28c261a8fc346de9a9
Nik Nyby <nikolas@gnu.org>
2016-05-11 21:49:54 -0400

    add flexslider

---
+++ js/start.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    // Vimeo API nonsense
+    var player = document.getElementById('player_1');
+    $f(player).addEvent('ready', ready);
+
+    function addEvent(element, eventName, callback) {
+        if (element.addEventListener) {
+            element.addEventListener(eventName, callback, false)
+        } else {
+            element.attachEvent(eventName, callback, false);
+        }
+    }
+
+    function ready(player_id) {
+        var froogaloop = $f(player_id);
+        froogaloop.addEvent('play', function(data) {
+            $('.flexslider').flexslider("pause");
+        });
+        froogaloop.addEvent('pause', function(data) {
+            $('.flexslider').flexslider("play");
+        });
+    }
+
+    // Call fitVid before FlexSlider initializes, so the proper initial height can be retrieved.
+    $(".flexslider")
+        .flexslider({
+            animation: 'slide',
+            animationLoop: false,
+            smoothHeight: true,
+            video: true,
+            //itemWidth: 3,
+            slideshow: false,
+            prevText: '',
+            nextText: '',
+            before: function(slider){
+                $f(player).api('pause');
+            }
+        });
+});
